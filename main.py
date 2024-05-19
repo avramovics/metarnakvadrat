823af7c16c4c352b4900ca7e56c536ce5a521fd6
Stefan.Avramovic <stefan.avramovic883@gmail.com>
2024-05-19 11:39:56 +0200

    2

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"message": "Hello, World pipeline this is a new shiiish"}
+    return {"message": "Hello, World pipeline this is a new shiiish 2 "}
