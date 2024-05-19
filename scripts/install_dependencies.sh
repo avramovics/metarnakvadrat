96240669b95ae66c77e039dad40c03aabab88090
Stefan.Avramovic <stefan.avramovic883@gmail.com>
2024-05-19 11:21:55 +0200

    try

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Install necessary dependencies
-sudo apt-get update
-sudo apt-get install -y python3-pip
+#sudo apt-get update
+#sudo apt-get install -y python3-pip
 pip3 install -r /home/ubuntu/myapp/requirements.txt
