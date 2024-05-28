49526c75f40784e5db1828d47bf019352c54b5aa
Stefan.Avramovic <stefan.avramovic883@gmail.com>
2024-05-28 10:51:51 +0200

    install script updated

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Install necessary dependencies
+cd ~
 python3 -m venv venv
 source myenv/bin/activate
-sudo apt-get update
-sudo apt-get install -y python3-pip
 cd myapp
-sudo pip install -r requirements.txt
+pip install -r requirements.txt
+deactivate
