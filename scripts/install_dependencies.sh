87e471b3f1af776a135101ed0b4445ae5fc558ae
Stefan.Avramovic <stefan.avramovic883@gmail.com>
2024-05-29 18:34:22 +0200

    ss

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Install necessary dependencies
 cd ~
-python3 -m venv venv
-source venv/bin/activate
-cd myapp
-pip install -r requirements.txt
+#python3 -m venv venv
+#source venv/bin/activate
+#cd myapp
+#pip install -r requirements.txt
 #deactivate
