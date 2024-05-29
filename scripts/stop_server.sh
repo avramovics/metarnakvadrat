f66bd679aa5a855a2ff9a9b8d17220ae70d1a41a
Stefan.Avramovic <stefan.avramovic883@gmail.com>
2024-05-29 08:24:09 +0200

    ~

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -5,6 +5,8 @@ COMMAND="uvicorn main:app --host 0.0.0.0 --port 8000"
 
 if pgrep -f "$COMMAND" > /dev/null; then
     echo "Stopping server..."
+    cd ~
+    cd myapp
     pkill -f "$COMMAND"
     echo "Server stopped successfully."
 else
